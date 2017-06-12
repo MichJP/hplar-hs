e16a0c99790748c97af701d048561267ec44a159
Michael Park <mjparksd@gmail.com>
2017-06-12 02:05:33 -0700

    Add pretty printer for constant value

---
+++ run.sh
@@ -0,0 +1,1 @@
+stack exec hplar-hs-exe
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+data Formula a = Const Bool
+
+prettyPrint :: Formula a -> String
+prettyPrint (Const value) = show value
+
 someFunc :: IO ()
-someFunc = putStrLn "True"
+someFunc = putStrLn . prettyPrint $ Const True
