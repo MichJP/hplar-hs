a787f449de7fd9493a767b6d6e45c136bd407924
Michael Park <mjparksd@gmail.com>
2017-06-12 23:18:05 -0700

    Parse "True"

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,24 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Lib
     ( someFunc
     ) where
 
+import Text.Parsec
+import Text.Parsec.String
+import Str
+
 data Formula a = Const Bool
+
+statement :: GenParser Char st (Formula String)
+statement =
+  do string "True"
+     return (Const True)-- <|> string "False"
+--     return (Const False)
 
 prettyPrint :: Formula a -> String
 prettyPrint (Const value) = show value
 
 someFunc :: IO ()
-someFunc = putStrLn . prettyPrint $ Const True
+someFunc = case (parse statement "" [str|True|]) of
+  Right st -> putStrLn (prettyPrint st)
+  Left _ -> putStrLn "Error"
