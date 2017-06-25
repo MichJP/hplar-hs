62e8096f2946cc16ff4bdfcb99e03b0b7dbc3c56
Michael Park <mjparksd@gmail.com>
2017-06-25 01:30:58 -0700

    Add test cases for binary logic operators

--- tests/Test.hs
+++ tests/Test.hs
@@ -5,9 +5,25 @@ import Text.Megaparsec
 import Lib
 
 main :: IO ()
-main = void (runTestTT tests)
+main = void (runTestTT (TestList tests))
 
-test1 = TestCase (assertEqual "" (eval <$> (parse statement "" "True")) (Right True))
-test2 = TestCase (assertEqual "" (eval <$> (parse statement "" "False")) (Right False))
+mkTest expr expected = TestCase (assertEqual "" (eval <$> (parse statement "" expr)) (Right expected))
 
-tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
+test1 = TestLabel "test1" $ mkTest "True" True
+test2 = TestLabel "test2" $ mkTest "False" False
+test3 = TestLabel "test3" $ mkTest "  True" True
+test4 = TestLabel "test4" $ mkTest "True and True" True
+test5 = TestLabel "test5" $ mkTest "True and False" False
+test6 = TestLabel "test6" $ mkTest "False and True" False
+test7 = TestLabel "test7" $ mkTest "False and False" False
+test8 = TestLabel "test8" $ mkTest "True or True" True
+
+tests = [ test1
+        , test2
+        , test3
+        , test4
+        , test5
+        , test6
+        , test7
+        , test8
+        ]
