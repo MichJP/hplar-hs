ee8fdb0b2acf63b9837d734434ff3be6edda9b84
Michael Park <mjparksd@gmail.com>
2017-06-24 23:54:52 -0700

    Run unit tests from stack

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,7 +1,11 @@
+import Control.Monad (void)
 import Test.HUnit
 import Text.Megaparsec
 
 import Lib
+
+main :: IO ()
+main = void (runTestTT tests)
 
 test1 = TestCase (assertEqual "" (eval <$> (parse statement "" "True")) (Right True))
 test2 = TestCase (assertEqual "" (eval <$> (parse statement "" "False")) (Right False))
